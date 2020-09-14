94013e6ad1f9ef3364f49355ba55cdc9b024e2f4
Roshy <roshy@roshys-air.home>
2020-09-14 20:00:07 +0100

    sudo bad. real bad

--- Scripts/miniconda_install.sh
+++ Scripts/miniconda_install.sh
@@ -4,11 +4,12 @@
 if ! command -v conda &> /dev/null
 then
     echo "Conda could not be found, so installing..."
-    echo "Installing Miniconda on the EC2 Instances"
+    echo "Installing Miniconda on the EC2 Instance"
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh;
     echo 'Install miniconda';
-    /miniconda.sh -b -p -u $HOME/miniconda;
-    eval "$(/miniconda/bin/conda shell.bash hook)";
+    sudo chmod 777 /miniconda.sh
+    sudo /miniconda.sh -b -p $HOME/miniconda -u;
+    eval "$(~/miniconda/bin/conda shell.bash hook)";
 else
     echo "Conda is already installed"
 fi
@@ -17,6 +18,3 @@ fi
 echo "Activating conda environment"
 conda env create --file /python/outbound/environmental_droplet.yml
 echo "Finished"
-
-
-
