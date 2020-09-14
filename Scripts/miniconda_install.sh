13aade321709311ebeeeccd80c8bf6325a501ef9
Roshy <roshy@roshys-air.home>
2020-09-14 18:58:48 +0100

    attempt to install miniconda for any code deployed to ec2 instances

--- Scripts/miniconda_install.sh
+++ Scripts/miniconda_install.sh
@@ -1,5 +1,21 @@
-echo "Installing Miniconda on the EC2 Instances"
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh;
-echo 'Install miniconda';
-sh ~/miniconda.sh -b -p $HOME/miniconda;
-eval "$(~/miniconda/bin/conda shell.bash hook)";
+# Install / Check that Conda is installed
+if ! command -v conda &> /dev/null
+then
+    echo "Conda could not be found, so installing..."
+    echo "Installing Miniconda on the EC2 Instances"
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh;
+    echo 'Install miniconda';
+    sh ~/miniconda.sh -b -p $HOME/miniconda;
+    eval "$(~/miniconda/bin/conda shell.bash hook)";
+else
+    echo "Conda is already installed"
+fi
+
+# Lets activate the conda environment
+echo "Activating conda environment"
+cd /python
+conda env create --file environmental_droplet.yml
+echo "Finished"
+
+
+
