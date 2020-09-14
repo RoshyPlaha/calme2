ec4065ecd6e7e7508f88f72af56a9457e565b20c
Roshy <roshy@roshys-air.home>
2020-09-14 19:37:27 +0100

    corrected path for droplet

--- Scripts/miniconda_install.sh
+++ Scripts/miniconda_install.sh
@@ -7,16 +7,15 @@ then
     echo "Installing Miniconda on the EC2 Instances"
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh;
     echo 'Install miniconda';
-    sh ~/miniconda.sh -b -p -u $HOME/miniconda;
-    eval "$(~/miniconda/bin/conda shell.bash hook)";
+    /miniconda.sh -b -p -u $HOME/miniconda;
+    eval "$(/miniconda/bin/conda shell.bash hook)";
 else
     echo "Conda is already installed"
 fi
 
 # Lets activate the conda environment
 echo "Activating conda environment"
-cd /python/outbound
-conda env create --file environmental_droplet.yml
+conda env create --file /python/outbound/environmental_droplet.yml
 echo "Finished"
 
 
